6aab50bc3406977ea07318f38a41640fbe27df24
aedorado <annurag94@gmail.com>
2022-05-12 23:03:24 +0530

    Arrays & Functions

--- js/6-functions.js
+++ js/6-functions.js
@@ -121,11 +121,11 @@ function A(x) { // x = 1
             console.log(x + y + z); // x, y, z -> 1 + 2 + 3 = 6;
         }
         C(3);
-        console.log(z);
+        // console.log(z);
     }
     B(2);
 }
-A(1);
+// A(1);
 
 // sayHello()();   // sh ()
 
@@ -142,10 +142,116 @@ function fact(n) {
 // console.log(fact(6));
 
 
-function add (x = 8, y, z = 0) {
-    return x + y + z;
-}
+// function add (x = 8, y, z = 0) {
+//     return x + y + z;
+// }
 // console.log(add(2, 1));
+
+
+// Arrow functions
+
+// traditiona function
+// function () {
+//     console.log('hello');
+// }
+
+// function expression
+// let hello = function () {
+//     console.log('hello');
+// }
+// hello();
+
+// arrow function
+let hello = () => {
+    console.log('hello');
+}
+// hello();
+
+// let print = function(arg) {
+    // console.log(arg);
+// }
+
+let print = (arg) => {
+    console.log(arg);
+}
+// print(123);
+
+// function sum(a, b) {
+//     console.log(a + b);
+// }
+
+// let sum = function (a, b) {
+//     console.log(a + b);
+// }
+
+// let sum = (a, b) => {
+//     console.log(a + b);
+// } 
+
+// if the body has only one line, we can skip the curly brackets around the body
+// if the only line in the function returns a value, we can skip the return keyword
+
+let sum = (a, b) => a + b;
+
+// console.log(sum(3, 5));
+
+// Currying
+// A concept of fucntional programming
+// functional languages -> everything is a function
+// functional langages are based on lambda calculus
+// currying is aconcept of lambda calculus
+
+// other programming languages (C, C++, Java etc) they are based on computer architechture
+
+// JS is somewhere in between
+// First class functions is a concept of functional languages
+// Objects are a concept of OOP languages
+
+// Curring transforms a function
+// f(a, b, ... n) -> f(a)(b)(c) ... (n)
+// f(a)(b)(c) -> fa(b)(c) -> fab(c)...(n) ... -> F(n) -> result
+
+// Currying decomposes functions with multiple arguments
+// into a sequence of functions that accept one argument at a time
+
+// non-curry
+let add = function (a, b, c) {
+    return a + b + c;
+}
+// console.log(add(1, 2, 3));
+
+// let addCurry = function (a) {
+//     return function B(b) {
+//         return a + b;
+//     };
+// }
+// console.log(addCurry(10));  
+// return value of addCurry(10) -> function B(b) { return a + b};    a = 10;
+
+
+
+
+console.log(addCurry(10)(5));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // closures
@@ -162,4 +268,8 @@ function add (x = 8, y, z = 0) {
 // Array
 // Date
 // object
+
+
+
+
 
---
+++ js/9-arrays.js
@@ -0,0 +1,142 @@
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
+
+// Arrays enable us to use the same variable to access many values
+// Array can contain elements of different types
+
+//         0  1  2     3    4
+// let arr = [1, 5, 123, 3.4, "this string"];    // 5
+// console.log(arr);
+
+// length of array = number of elements in the array
+// console.log(arr[3]);
+
+// array length = l
+// insert @ -> l + n , then the array length becomes  l + n + 1
+
+// let a = [];
+// // console.log(a, a.length);
+// a[0] = 4;   // insert 4 @ postion 0
+// // java will give indexoutofbound
+// a[1] = 5;
+// a[2] = 8;
+// a[5] = 4;
+// a[5] = 404;
+// a[1] = 10;
+// console.log(a, a.length, typeof a);
+// console.log("last ele: ", a[a.length - 1])
+
+// console.log('for of ...')
+// for (let x of a) {
+//     console.log(x);
+// }
+
+// console.log(a[1000]);
+// console.log(a.length);
+
+// let f = function(ele) {
+//     console.log(ele * 10);
+// }
+
+
+
+
+
+
+
+let f = function (a) {
+    console.log(a);
+}
+
+
+// let arr = [5, 7, 1, 0, 6, 3, 4, 5];
+// // arr.forEach(ele => console.log(ele));
+// arr.forEach((ele, i) => {
+//     console.log(ele);
+//     console.log(ele);
+// });
+
+
+// arr.forEach((ele, i, arr) => {
+    // console.log(ele);
+// });
+// arr.forEach((ele, i, third_arg, fourth, fifth) => {
+//     console.log(`Array arr contains value=${ele} @ index=${i}`);
+// //    console.log(ele, i);
+// }); // for each element of the array arr, apply the function f on it
+
+// Anonymous function
+
+
+// f(5) -> 5
+// f(7) -> 7
+// f(1) -> 1
+// ...
+// f(5) -> 5
+
+// for (var x of arr) {  // x = 5    7   1   0   6   3   4   5   .. loop ends
+//     console.log(x);
+// }
+
+
+// Add elements to end or array
+// Remove lement from the end of array
+let arr = ["apple", "bat", 45, "cat", "dog", 123];
+
+// unshift -> push from start; returns the length of the new array
+// console.log(arr.unshift("apricot"));
+// console.log(arr.unshift("alpha"));
+// console.log(arr.unshift("aeroplane"));
+// console.log(arr);
+
+// shift -> pop from start; returns the removed element
+let sv = arr.shift();
+// console.log(sv, arr);
+sv = arr.shift()
+// console.log(sv, arr);
+let a1 = [1, 3];
+let a2 = [4, 6];
+let a3 = [7];
+let a4 = [10, 11];
+let a5 = a1.concat(a2, a3, a4); 
+// a4 = a1 + a2
+// let a5 = a4.concat(a3);
+console.log(a1, a2, a3, a4);
+console.log(a5);
+
+
+// console.log(arr.toString());
+// console.log(arr.join(" / "))
+// arr[arr.length] = "new element";
+// arr[arr.length] = "new element 1";
+// arr[arr.length] = "new element 2";
+arr.push("last element 1");
+arr.push("last element 2");
+// console.log(arr);
+arr.pop();
+arr.pop();
+arr.pop();
+// console.log(arr);
+
+// console.log(typeof arr);
+// console.log(typeof 1);
+// console.log(Array.isArray(arr));
+
+
+// SPLICE
+// SLICE
+// SORTING
+// 
+
+
+
+// roz ka roz - revise
+// 
+
+
+
+
+
+
+
+
+
